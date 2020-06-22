d0726e6038902b7b21e8c889c416400ccfcd4a49
Nicolas KAROLAK <nicolas@karolak.fr>
2020-06-22 07:15:01 +0000

    rename nextcloud -> app

--- main.tf
+++ main.tf
@@ -1,3 +1,90 @@
+# BACKEND
+
+terraform {
+  backend "s3" {
+    endpoint                    = "https://s3.fr-par.scw.cloud"
+    region                      = "fr-par"
+    bucket                      = "karolak-tfstate-bucket"
+    key                         = "infra.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+# PROVIDERS
+
+provider "scaleway" {}
+
+provider "ovh" {}
+
+# INSTANCES
+
+module "app_instance" {
+  source = "./modules/scw-instance"
+  name   = "app"
+  tags   = ["grocy", "nextcloud", "postgresql_server", "private", "redis", "rssbridge", "wallabag"]
+}
+
+module "grocy_instance" {
+  source = "./modules/scw-instance"
+  name   = "grocy"
+  tags   = ["private"]
+}
+
+module "lb_instance" {
+  source  = "./modules/scw-instance"
+  name    = "lb"
+  type    = "START1-XS"
+  tags    = ["public"]
+  ipv4_id = module.lb_ip.id
+}
+
+module "smtp_instance" {
+  source  = "./modules/scw-instance"
+  name    = "smtp"
+  type    = "START1-XS"
+  tags    = ["imap", "postgresql_server", "public", "redis", "rspamd"]
+  ipv4_id = module.mail_ip.id
+}
+
+module "wallabag_instance" {
+  source = "./modules/scw-instance"
+  name   = "wallabag"
+  tags   = ["postgresql_server", "private", "redis"]
+}
+
+module "web_instance" {
+  source = "./modules/scw-instance"
+  name   = "web"
+  tags   = ["mariadb", "private", "redis"]
+}
+
+# IP
+
+module "lb_ip" {
+  source  = "./modules/scw-ip"
+  name    = "lb"
+  aliases = ["bag", "blog", "cloud", "crypto", "git", "grocy", "mail", "ndata", "pfa", "rspamd", "rssbr", "wedding", "www"]
+}
+
+module "mail_ip" {
+  source  = "./modules/scw-ip"
+  name    = "smtp"
+  aliases = ["imap"]
+}
+
+# STORAGE
+
+resource "scaleway_object_bucket" "nextcloud" {
+  name = "karolak-nextcloud-data-bucket"
+  acl  = "private"
+}
+
+resource "scaleway_object_bucket" "backup" {
+  name = "karolak-backup-bucket"
+  acl  = "private"
+}
+
 # DNS
 
 resource "ovh_domain_zone_record" "apex" {
